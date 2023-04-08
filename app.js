6d200ee5a79d7caa0bb281646d55716cb1efa024
shakirat <shakiratganiyy@gmail.com>
2023-04-08 22:16:38 +0100

    Updated the HTML, CSS and added images

---
+++ app.js
@@ -0,0 +1,11 @@
+const timesToRepeat = 100;
+const character = "💃";
+//let answer = "";
+
+//for (let i = 0; i < timesToRepeat; i++) {
+//  answer = answer + character;
+//}
+
+//console.log(answer);
+
+console.log("".padStart(timesToRepeat, character));
